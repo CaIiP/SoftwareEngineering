c95227bd33d8aedf3668a8ad9654e233567c61b6
ShannenHarper <2095119H@student.gla.ac.uk>
2020-02-24 18:01:37 +0000

    staff class

--- src/staff/Staff.java
+++ src/staff/Staff.java
@@ -1,5 +1,38 @@
 package staff;
 
 public class Staff {
-
+	private int id;
+	private String name;
+	private int dob;
+	private String contact;
+	private String education;
+	private String spec;
+	
+	public Staff(int a, String b, int c, String d, String e, String f) {
+		this.id = a;
+		this.name = b;
+		this.dob = c;
+		this.contact = d;
+		this.education = e;
+		this.spec = f;		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getDob() {
+		return dob;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public String getSpec() {
+		return spec;
+	}
 }
